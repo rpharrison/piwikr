7e4dc23363dd75e981a607bcbceb1a44879d33b6
Richard Harrison <richard@richardharrison.es>
2016-05-13 16:20:44 +0200

    Update data_retrieval.R

--- R/data_retrieval.R
+++ R/data_retrieval.R
@@ -31,12 +31,12 @@ describe_database <- function(db) {
 }
 
 get_actions <- function(db) {
-    actions <- .get(db, prefix, 'piwik_log_link_visit_action')
+    actions <- .get(db, prefix'piwik_log_link_visit_action')
 
     actions <- .remove_empty_columns(actions)
 
     ## Set up metadata on action types
-    action_types <- .get(db, prefix, 'piwik_log_action')
+    action_types <- .get(db, prefix'piwik_log_action')
     path <- system.file("extdata", "action_types.csv", package="piwikr")
     metadata <- read.csv(path)
     action_types <- action_types %>% left_join(metadata, by=c('type'='id'))
@@ -65,7 +65,7 @@ get_actions <- function(db) {
 }
 
 get_visits <- function(db) {
-    visits <- .get(db, prefix, 'piwik_log_visit')
+    visits <- .get(db, prefix'piwik_log_visit')
     visits <- .remove_empty_columns(visits)
 
     visits$visit_first_action_time <- lubridate::ymd_hms(visits$visit_first_action_time)
